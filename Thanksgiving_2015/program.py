9e2825d1ffe1846a9b10a80d516fe07413d2f614
Benson <bigbuzi@gmail.com>
2019-10-22 15:01:48 -0400

    troubleshoot pandas

--- Thanksgiving_2015/program.py
+++ Thanksgiving_2015/program.py
@@ -1,13 +1,11 @@
 import csv
 from pathlib import PurePath
 from collections import namedtuple
+import numpy as np
+import pandas as pd
 
 base_folder = PurePath(__file__).parent
 file_name = base_folder.joinpath('data', 'thanksgiving-2015-poll-data.csv')
 
-Record = namedtuple('Record')
-
-with open(file_name, 'r') as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        print(row.get())
+df = pd.read_csv(file_name)
+print(df.head(3))
