d32fe0fa178ae919dd1898287ca7a8cbac6513ae
Benson <bigbuzi@gmail.com>
2019-10-27 10:27:05 -0400

    added user prompt and random sampling

--- Thanksgiving_2015/program.py
+++ Thanksgiving_2015/program.py
@@ -8,12 +8,48 @@ base_folder = PurePath(__file__).parent
 file_name = base_folder.joinpath('data', 'thanksgiving-2015-poll-data.csv')
 
 raw_df = pd.read_csv(file_name)
-df_extr_cols = raw_df.iloc[:,np.r_[:2, 63:64, 11:24, 25, 26:37, 38, 40:49, 50]]
-col_names = ['income'] + (['sides'] * 14) + (['pie'] * 12) + (['desserts'] * 10)
-df_named = df_extr_cols.rename(columns=dict(zip(df1.columns[2:], col_names)))
-df = df_named.melt(id_vars=['RespondentID', 'Do you celebrate Thanksgiving?', 'income'], var_name='item', value_name='dish')
+
+#Extract relevant columns, categorize and unpivot data
+df_extr_cols = raw_df.iloc[:,np.r_[:2, 64, 63:64, 11:24, 25, 26:37, 38, 40:49, 50]]
+col_names = ['region'] + ['income'] +  (['sides'] * 14) + (['pie'] * 12) + (['desserts'] * 10)
+df_named = df_extr_cols.rename(columns=dict(zip(df_extr_cols.columns[2:], col_names)))
+df = df_named.melt(id_vars=['RespondentID', 'Do you celebrate Thanksgiving?', 'region', 'income'], var_name='item', value_name='dish')
 df.dropna(subset=['dish'], inplace=True)
 
-print(raw_df.head(3))
-print(raw_df.columns[11:50])
-print(raw_df.columns[64])
+
+def prompt_user_selection(regions=df.region.unique(), income=df.income.unique()):
+    region_options = dict(enumerate(regions, 1))
+    income_options = dict(enumerate(income, 1))
+    usr_selection = []
+    print('Select region from list below:')
+    #Region selection
+    for key, value in region_options.items():
+        print(f'{key}. {value}')
+    while True:
+        try:
+            i = int(input('Enter number: '))
+            usr_selection.append(region_options[i])
+            break
+        except ValueError:
+            print('Please enter valid number.')
+        except KeyError:
+            print('Please enter valid number from options above')
+    #Income selection
+    print('Select income category from list below:')
+    for key, value in income_options.items():
+        print(f'{key}. {value}')
+    while True:
+        try:
+            i = int(input('Enter number: '))
+            usr_selection.append(income_options[i])
+            break
+        except ValueError:
+            print('Please enter valid number.')
+        except KeyError:
+            print('Please enter valid number from options above')
+    
+    return usr_selection
+
+user_selection = prompt_user_selection()
+
+print(df[(df.region == user_selection[0]) & (df.income == user_selection[1])].sample(5)['dish'].tolist())
